352050ac606d3d3f4ee52befa577caa10b822b5d
YSFsemlali <ysfm.102@gmail.com>
2023-11-10 17:25:33 +0100

    'update commit'

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,12 +6,12 @@
 /*   By: ysemlali <ysemlali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 17:29:09 by ysemlali          #+#    #+#             */
-/*   Updated: 2023/11/09 18:34:44 by ysemlali         ###   ########.fr       */
+/*   Updated: 2023/11/10 17:22:47 by ysemlali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
+
 
 
 // int	main(void)
